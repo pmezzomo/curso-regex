0cf64049e3279950ec1df8f41cf4774dcc503465
PaulaMz <64453516+pmezzomo@users.noreply.github.com>
2022-03-31 23:11:06 -0300

    Create AlgunsCuidados.js

---
+++ caracteres/AlgunsCuidados.js
@@ -0,0 +1,10 @@
+const textoUmalinha = '...' //aspas simples ou duplas
+
+const textoMultiplasLinhas = `
+    linha 1
+    linha 2
+    Usa crase qdo tem multiplas linhas
+`
+
+//cuidado com o tab!
+console.log('	'.match(/\s/g))
